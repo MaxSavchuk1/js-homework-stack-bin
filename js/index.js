57f4aa504f71e039262c62299f7deebd8addd6d7
MaxSavchuk <avatar2945@gmail.com>
2021-06-08 20:26:31 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+class Node {
+  constructor(data, next) {
+    this._data = data;
+    this._next = next;
+  }
+}
+
+class Stack {
+  constructor(maxSize = 99) {
+    this._maxSize = maxSize;
+    this._size = 0;
+    this._top = null;
+  }
+  push(value) {
+    if (this._size === this._maxSize) {
+      throw new RangeError('Stack overflow');
+    }
+    this._top = new Node(value, this._top);
+    this._size++;
+  }
+  pop() {
+    if (!this.isEmpty) {
+      const value = this._top._data;
+      this._top = this._top._next;
+      this._size--;
+      return value;
+    }
+  }
+  get peek() {
+    return this._top?._data;
+  }
+  get isEmpty() {
+    return this._size === 0;
+  }
+}
---
+++ js/stack.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//в файле index.js лежат классы для стека
+
+function isPalindrome(str) {
+  const palindrome = new Stack();
+  for (const s of str) {
+
+  }
+  return palindrome;
+}
