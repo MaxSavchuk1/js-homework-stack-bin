32345b0612f7216f564a4419c858039be4aa4dc3
MaxSavchuk <avatar2945@gmail.com>
2021-06-13 13:37:08 +0300

    Re:Re: edit

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,24 +1,26 @@
 "use strict";
 
-const numberInMind = 0; // задуманное число
+// guessNumber(); 
 
 function guessNumber() {
+  alert("загадайте число от 0 до 100");
   let start = 0;
   let end = 100;
-  
   do {
     let middle = Math.floor((start + end) / 2);
     let isCorrect = confirm(`Ваше число меньше или равно ${middle} ?`)
-    isCorrect ? end = middle : start = middle;
-    if (middle === numberInMind) {
-      return middle;
-    }
-  } while (start !== numberInMind || end !== numberInMind);
-  return -1; // почему -1? я не помню :(
+    if (isCorrect) {
+     end = middle;
+     let isGuess = confirm(`Ваше число ${middle} ?`)
+      if (isGuess) {
+        break;
+      }
+     } else {
+       start = middle;
+     }
+  } while (start + 1 !== end);
+  return alert(`Ваше число: ${end}`);
 }
 
-// guessNumber();
 
-// если ошибиться и нажать неверно в confirm, хотя оно верно, то бесконечный цикл. Надо доделать
-// хотя, будем считать, что пользователь не глупый
-// ну да :DDD
+
